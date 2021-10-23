ef9a034df7276a46463770a1734284c0d11c391d
Lucas Moschen <lucas.machadomoschen@gmail.com>
2021-10-22 23:04:24 -0300

    increasing efficience

--- exercises/exercise_5_ps2/exercise_5_ps2.py
+++ exercises/exercise_5_ps2/exercise_5_ps2.py
@@ -21,14 +21,12 @@ def initiate_model():
     return stan_model 
 
 def binomial_product(y, z, m, n, theta1, theta2):
-    pdf = np.log(comb(n, y)) +  y*np.log(theta2) + (n-y)*np.log1p(-theta2) 
-    pdf += np.log(comb(m, z-y)) +  (z-y)*np.log(theta1) + (m-z+y)*np.log1p(-theta1)
-    return np.exp(pdf)
+    return comb(n, y) * comb(m, z-y) * (theta2*(1-theta1)/(theta1*(1-theta2)))**y
 
 def probabilities_binomial_product(z, m, n, theta1, theta2):
     p = np.zeros(min(n, z) - max(0, z-m)+1)
-    i=-1
-    for y in range(max(0, z-m), min(n, z)+1): 
+    i = -1
+    for y in range(max(0, z-m), min(n, z)+1):
         i += 1
         p[i] = binomial_product(y, z, m, n, theta1, theta2)
     return p/sum(p)
@@ -39,26 +37,23 @@ def binomial_product_rng(z, m, n, theta1, theta2):
     index = np.argmax(u < np.cumsum(p))
     return index + max(0,z-m)
 
-def gibbs_sampler(z, m, n, T, warmup, iterations):
-    x = np.zeros(T)
-    y = np.zeros(T)
+def gibbs_sampler_iteration(y, z, m, n, T, theta1, theta2):
+    for i in range(T):
+        y[i] = binomial_product_rng(z[i], m[i], n[i], theta1, theta2)
+    theta1 = np.random.beta(1 + sum(z-y), 1 + sum(m-z+y))
+    theta2 = np.random.beta(1 + sum(y), 1 + sum(n-y))
+    return theta1, theta2
 
+def gibbs_sampler(z, m, n, T, warmup, iterations):
+    y = np.zeros(T)
     theta1 = np.random.uniform()
     theta2 = np.random.uniform()
-    for _ in tqdm(range(warmup)): 
-        for i in range(T): 
-            y[i] = binomial_product_rng(z[i], m[i], n[i], theta1, theta2)
-            x[i] = z[i] - y[i]
-        theta1 = np.random.beta(1 + sum(x), 1 + sum(m-x))
-        theta2 = np.random.beta(1 + sum(y), 1 + sum(n-y))
+    for _ in tqdm(range(warmup), desc='Warmup'):
+        theta1, theta2 = gibbs_sampler_iteration(y, z, m, n, T, theta1, theta2)
 
     samples = np.zeros((iterations, 2))
-    for ite in tqdm(range(iterations)):
-        for i in range(T): 
-            y[i] = binomial_product_rng(z[i], m[i], n[i], theta1, theta2)
-            x[i] = z[i] - y[i]
-        theta1 = np.random.beta(1 + sum(x), 1 + sum(m-x))
-        theta2 = np.random.beta(1 + sum(y), 1 + sum(n-y))   
+    for ite in tqdm(range(iterations), desc='Sampling'):
+        theta1, theta2 = gibbs_sampler_iteration(y, z, m, n, T, theta1, theta2)
         samples[ite, 0] = theta1
         samples[ite, 1] = theta2
     return samples
@@ -82,10 +77,20 @@ def main():
 
     sampling = gibbs_sampler(Z, m, n, len(m), warmup=2000, iterations=1000)
 
-    print(sampling[:, 0].mean())
-    print(fitting.extract()['theta1'].mean())
-    print(sampling[:, 1].mean())
-    print(fitting.extract()['theta2'].mean())
+    print("Gibbs theta1 mean: {}".format(sampling[:, 0].mean()))
+    print("Stan theta1 mean: {}".format(fitting.extract()['theta1'].mean()))
+    print("Gibbs theta2 mean: {}".format(sampling[:, 1].mean()))
+    print("Stan theta2 mean: {}".format(fitting.extract()['theta2'].mean()))
+
+    print("Gibbs theta1 25-q: {}".format(np.quantile(sampling[:, 0], q=0.25)))
+    print("Stan theta1 25-q: {}".format(np.quantile(fitting.extract()['theta1'], q=0.25)))
+    print("Gibbs theta2 25-q: {}".format(np.quantile(sampling[:, 1], q=0.25)))
+    print("Stan theta2 25-q: {}".format(np.quantile(fitting.extract()['theta2'], q=0.25)))
+
+    print("Gibbs theta1 75-q: {}".format(np.quantile(sampling[:, 0], q=0.75)))
+    print("Stan theta1 75-q: {}".format(np.quantile(fitting.extract()['theta1'], q=0.75)))
+    print("Gibbs theta2 75-q: {}".format(np.quantile(sampling[:, 1], q=0.75)))
+    print("Stan theta2 75-q: {}".format(np.quantile(fitting.extract()['theta2'], q=0.75)))
 
 if __name__ == '__main__':
 
